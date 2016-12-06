baab8ba6fc3b2f05a4968bc8a1efe675591b6854
Ayax Córdova <ayax.cordova@aydev.mx>
2016-12-05 21:44:04 -0600

    initial commit

---
+++ UPGRADE.md
@@ -0,0 +1,37 @@
+# Upgrade Guide
+
+## Upgrading To 7.0 From 6.x
+
+### Minimum Versions
+
+The following required dependency versions have been updated:
+
+- The minimum PHP version is now v8.0
+- The minimum Laravel version is now v9.0
+- The minimum PHPUnit version is now v9.0
+
+### Removed Chrome Binaries
+
+PR: https://github.com/laravel/dusk/pull/873
+
+Going forward, Dusk will not ship with pre-installed Chrome binaries. Instead, you should install the required Chrome driver for your operating system using the following command:
+
+```zsh
+php artisan dusk:chrome-driver
+```
+
+## Upgrading To 5.0 From 4.x
+
+### PHPUnit 8
+
+Dusk now provides optional support for PHPUnit 8, which requires PHP >= 7.2 Please read through the entire list of changes in [the PHPUnit 8 release announcement](https://phpunit.de/announcements/phpunit-8.html). Using PHPUnit 8 will require Laravel 5.8, which will be released at the end of February 2019.
+
+You may also continue using PHPUnit 7, which requires a minimum of PHP 7.1.
+
+### Minimum Laravel version
+
+Laravel 5.7 is now the minimum supported version of the framework and you should upgrade to continue using Dusk.
+
+### `setUp` and `tearDown` changes
+
+The `setUp` and `tearDown` methods now require the `void` return type. If you were overwriting these methods you should add it to the method signatures.
---
+++ tests/ChromeProcessTest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Laravel\Dusk\Tests;
+
+use Laravel\Dusk\Chrome\ChromeProcess;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Symfony\Component\Process\Process;
+
+class ChromeProcessTest extends TestCase
+{
+    public function test_build_process_with_custom_driver()
+    {
+        $driver = __DIR__;
+
+        $process = (new ChromeProcess($driver))->toProcess();
+
+        $this->assertInstanceOf(Process::class, $process);
+        $this->assertStringContainsString("$driver", $process->getCommandLine());
+    }
+
+    public function test_build_process_for_windows()
+    {
+        try {
+            (new ChromeProcessWindows)->toProcess();
+        } catch (RuntimeException $exception) {
+            $this->assertStringContainsString('chromedriver-win.exe', $exception->getMessage());
+        }
+    }
+
+    public function test_build_process_for_darwin_intel()
+    {
+        try {
+            (new ChromeProcessDarwinIntel)->toProcess();
+        } catch (RuntimeException $exception) {
+            $this->assertStringContainsString('chromedriver-mac-intel', $exception->getMessage());
+        }
+    }
+
+    public function test_build_process_for_darwin_arm()
+    {
+        try {
+            (new ChromeProcessDarwinArm)->toProcess();
+        } catch (RuntimeException $exception) {
+            $this->assertStringContainsString('chromedriver-mac-arm', $exception->getMessage());
+        }
+    }
+
+    public function test_build_process_for_linux()
+    {
+        try {
+            (new ChromeProcessLinux)->toProcess();
+        } catch (RuntimeException $exception) {
+            $this->assertStringContainsString('chromedriver-linux', $exception->getMessage());
+        }
+    }
+
+    public function test_invalid_path()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Invalid path to Chromedriver [/not/a/valid/path]. Make sure to install the Chromedriver first by running the dusk:chrome-driver command.');
+
+        (new ChromeProcess('/not/a/valid/path'))->toProcess();
+    }
+}
+
+class ChromeProcessWindows extends ChromeProcess
+{
+    protected function onMac()
+    {
+        return false;
+    }
+
+    protected function onWindows()
+    {
+        return true;
+    }
+
+    protected function operatingSystemId()
+    {
+        return 'win';
+    }
+}
+
+class ChromeProcessDarwinIntel extends ChromeProcess
+{
+    protected function onMac()
+    {
+        return true;
+    }
+
+    protected function onWindows()
+    {
+        return false;
+    }
+
+    protected function operatingSystemId()
+    {
+        return 'mac-intel';
+    }
+}
+
+class ChromeProcessDarwinArm extends ChromeProcess
+{
+    protected function onMac()
+    {
+        return true;
+    }
+
+    protected function onWindows()
+    {
+        return false;
+    }
+
+    protected function operatingSystemId()
+    {
+        return 'mac-arm';
+    }
+}
+
+class ChromeProcessLinux extends ChromeProcess
+{
+    protected function onArmMac()
+    {
+        return false;
+    }
+
+    protected function onIntelMac()
+    {
+        return false;
+    }
+
+    protected function onWindows()
+    {
+        return false;
+    }
+
+    protected function operatingSystemId()
+    {
+        return 'linux';
+    }
+}
